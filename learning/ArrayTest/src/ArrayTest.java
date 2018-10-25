3bd0bd0033f92d2eda612b1fb4f83000701ac8e6
Nazmul Basher <nazmulbasher@Nazmuls-MacBook-Air.local>
2018-10-25 12:59:05 +0600

    Added some codes

--- learning/ArrayTest/src/ArrayTest.java
+++ learning/ArrayTest/src/ArrayTest.java
@@ -9,14 +9,22 @@
  * @author nazmulbasher
  */
 public class ArrayTest {
-    public static void main(String args[]){
-        int []numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    public static void main(String[] args){
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         
         for(int number : numbers){
             number = (number%2==0) ? 0 : number;
             
             if(number>0)
                 System.out.println(number);
+        }
+        
+        System.out.println("----");
+        
+        double[] myList = {1.9, 2.9, 3.4, 3.5};
+
+        for (int i = 0; i < myList.length; i++) {
+           System.out.println(myList[i] + " ");
         }
     }
 }
